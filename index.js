b51b5b4c20e2c9f022d827d411c8794b216d85a3
dayatz <dayat.py@gmail.com>
2015-04-25 11:29:10 +0800

    change listening host

--- index.js
+++ index.js
@@ -31,5 +31,5 @@ app.get('/ela', function(req, res){
 var port =  process.env.OPENSHIFT_NODEJS_PORT || 3000;   // Port 8080 if you run locally
 var host =  process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1"; // Listening to localhost if you run locally
 
-app.listen(port, address);
+app.listen(port, host);
 console.log('Server running on http://127.0.0.1:3000');
